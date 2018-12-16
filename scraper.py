a5cccba9a651b33b67579a404ba10b3170681a49
aaronmccormack15533 <maron.accormack@gmail.com>
2018-12-16 20:49:12 +0000

    scrape writing to file

--- scraper.py
+++ scraper.py
@@ -11,22 +11,26 @@ pageResp = pageRequest.content.decode('ISO-8859-1', 'replace')
 soup = BeautifulSoup(pageResp, 'html.parser')
 calender = soup.find('table', {'class': 'musicTable'})
 
+file = open('bin/UpcomingAlbums/upcomingRelease.txt', 'w')
+
 for tr in calender.find_all('tr'):
     if tr.find('a') is None:
         if tr.find('td') is None:
             continue
         else:
-            artistName = tr.find('td', {'class': 'artistName'})
-            artistName.encode('utf-8')
-            albumName = tr.find('td', {'class': 'albumTitle'})
-            albumName.encode('utf-8')
+            artistName = tr.find('td', {'class': 'artistName'}).text.strip()
+            albumName = tr.find('td', {'class': 'albumTitle'}).text.strip()
+            
     else:
-        artistName = tr.find('a')
-        artistName.encode('utf-8')
-        albumName = tr.find('td', {'class': 'albumTitle'})
-        albumName.encode('utf-8')
+        artistName = tr.find('a').text.strip()
+        albumName = tr.find('td', {'class': 'albumTitle'}).text.strip()
+        
         
     print(artistName)
+    file.writelines('%s\n' % artistName.encode('utf-8'))
     print(albumName)
+    file.writelines('%s\n' % albumName.encode('utf-8'))
+
+file.close()
 
 # print(soup.encode('utf-8'))
