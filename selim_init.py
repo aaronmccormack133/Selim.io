d088a89e42a46babe0cc6e746874b11284be2be4
aaronmccormack15533 <maron.accormack@gmail.com>
2019-04-27 17:04:07 +0100

    changed query method for sections

--- selim_init.py
+++ selim_init.py
@@ -12,28 +12,39 @@ tts.speak('Saylem activated')
 tts.speak("What would you like?")
 query = input()
 
-if((query == "similar artist") or (query == "similar artists")):
-    # similar artists
-    tts.speak('what artist')
-    artist = input()
-    tts.speak('how many results would you like')
-    limit = input()
-    tts.speak('artists that are similar to ' + artist + ' are...')
-    sa.call(artist, limit)
-elif((query == "upcoming concerts") or (query == "upcoming shows") or (query == "upcoming gigs")):
+similar_artist_keyword = ['similar', 'like', 'artist', 'artists']
+upcoming_shows_keyword = ['concert', 'shows', 'gig', 'gigs', 'show', 'event']
+upcoming_albums_keyword = ['albums', 'new', 'album', 'release', 'releases', 'records']
+
+# if(query in similar_artist_keyword):
+
+
+for i in query.split(' '):
+    if(i in similar_artist_keyword):
+        # similar artists
+        tts.speak('what artist')
+        artist = input()
+        tts.speak('how many results would you like')
+        limit = input()
+        tts.speak('artists that are similar to ' + artist + ' are...')
+        sa.call(artist, limit)
+    elif(i in upcoming_shows_keyword):
     # upcoming concerts
-    tts.speak('would you like all concerts or a specific artist')
-    concertType = input()
-    if((concertType == "all") or (concertType == "all concerts")):
+        tts.speak('would you like all concerts or a specific artist')
+        concertType = input()
+        if('all' in concertType):
         # getting all upcoming concerts
-        uc.concertCall('all')
-    else:
+            uc.concertCall('all')
+        else:
         # getting artist specific concerts
-        tts.speak('what artist would you like?')
-        artistCall = input()
-        uc.concertCall(artistCall)
-elif((query == "new albums") or (query == "new album releases") or ("upcoming albums")):
+            tts.speak('what artist would you like?')
+            artistCall = input()
+            uc.concertCall(artistCall)
+    elif(i in upcoming_albums_keyword):
     # getting new albums
-    tts.speak("Albums being released soon are")
-    time.sleep(1)
-    ua.upcoming()
+        tts.speak("Albums being released soon are")
+        time.sleep(1)
+        ua.upcoming()
+    else:
+        tts.speak('query not found')
+        
