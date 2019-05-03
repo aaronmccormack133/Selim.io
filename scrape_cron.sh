d8bd861d10257641411b883eaa41223884af79c4
aaronmccormack15533 <maron.accormack@gmail.com>
2019-05-03 03:54:05 +0100

    shell script to create anacron

---
+++ scrape_cron.sh
@@ -0,0 +1,9 @@
+chmod +x scraper.py
+touch selim-scrape
+echo "#!/bin/sh
+
+set -e
+
+./home/pi/Documents/Selim.io/scraper.py" >> selim-scrape
+chmod +x selim-scrape
+mv selim-scrape /etc/cron.weekly/
