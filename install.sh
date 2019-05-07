eb42522ca720c363f763e778ebc5e5d1e9d9430b
aaronmccormack15533 <maron.accormack@gmail.com>
2019-05-07 22:28:22 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+apt-get install update
+apt-get install espeak -y
+apt-get install python3-dev -y
+apt-get install pyaudio -y
+apt-get install update 
+
+pip3 install requests
+pip3 install BeautifulSoup4
+pip3 install pyttsx3
+pip3 install SpeechRecognition
+
+chmod +x scrape_cron.sh
+chmod +x selim_init.py
+./scrape_cron.sh
