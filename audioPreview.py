eb177352d1080a0d826e4c86e90eab2c812a44d7
aaronmccormack15533 <maron.accormack@gmail.com>
2019-05-09 21:42:26 +0100

    youtube player

--- audioPreview.py
+++ audioPreview.py
@@ -3,18 +3,20 @@ import urllib.request
 import urllib.parse
 import re
 from pytube import YouTube
+import subprocess
 
 # https://www.youtube.com/watch?v=oHg5SJYRHA0
 
 def getUrl(artistName):
-    scriptStart = "ffmpeg $(youtube-dl -g --extract-audio '"
-    scriptEnd = "' | sed \"s/.*/-ss 00:05 -i &/\") -t 0:30 -c copy out.mp4"
+    scriptStart = "ffmpeg -y $(youtube-dl -g --extract-audio '"
+    scriptEnd = "' | sed \"s/.*/-ss 00:10 -i &/\") -t 0:30 -c copy bin/AudioPreview/clip.mp4\ncvlc --play-and-exit bin/AudioPreview/clip.mp4"
     query = urllib.parse.urlencode({"search_query": artistName})
     page = urllib.request.urlopen("http://www.youtube.com/results?" + query)
     results = re.findall(r'href=\"\/watch\?v=(.{11})', page.read().decode())
     bashResp = "http://www.youtube.com/watch?v=" + results[1]
     with open('youtube.sh', 'w') as f:
-        f.writelines(scriptStart + bashResp + scriptEnd)
+        f.writelines("#!/bin/sh\n" + scriptStart + bashResp + scriptEnd)
+    subprocess.call(['./youtube.sh'])
     # return('http://www.youtube.com/watch?v=' + results[1])
 
 # def getMp3(url):
--- youtube.sh
+++ youtube.sh
@@ -1,1 +1,3 @@
-ffmpeg $(youtube-dl -g --extract-audio 'http://www.youtube.com/watch?v=M_5kdIhx_to' | sed "s/.*/-ss 00:05 -i &/") -t 0:30 -c copy out.mp4
+#!/bin/sh
+ffmpeg -y $(youtube-dl -g --extract-audio 'http://www.youtube.com/watch?v=M_5kdIhx_to' | sed "s/.*/-ss 00:10 -i &/") -t 0:30 -c copy bin/AudioPreview/clip.mp4
+cvlc --play-and-exit bin/AudioPreview/clip.mp4
